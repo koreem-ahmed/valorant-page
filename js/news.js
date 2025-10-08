3ded194ee25e7839ddc7eeb0d940bb80674e5b2e
karim ahmed abd elkareem <karim.1024112@stemoctober.moe.edu.eg>
2025-10-08 20:06:12 +0300

    Delete js/news.js

--- js/news.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    if (window.scrollY === 0) {
-        document.querySelector('.nav').classList.remove('blur');
-    } else if (window.scrollY > 0) {
-        document.querySelector('.nav').classList.add('blur');
-    }
-});
